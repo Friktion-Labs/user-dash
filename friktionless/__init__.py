08616e5e4ac9becff97f421647859da0d5b6fb98
Matt Martin <matt.martin@hey.com>
2022-04-05 02:21:11 -0700

    added friktionless.analytics

--- friktionless/__init__.py
+++ friktionless/__init__.py
@@ -72,3 +72,4 @@ $ (friktion) johnreyes@pop-os:~/projects/user-dash$ python
 import friktionless.friktion_user_table
 import friktionless.friktion_source_tables
 import friktionless.friktion_etl
+import friktionless.analytics
