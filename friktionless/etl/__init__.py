dc85f7de673b9029cb4285332506389820e5e8f7
Amarante <amar@mrnt.xyz>
2022-04-05 23:49:56 -0500

    clean up api pathway to create table function

--- friktionless/etl/__init__.py
+++ friktionless/etl/__init__.py
@@ -4,4 +4,4 @@ The ETL API provides hardened data warehouse hydration in code.
 The api is designed for easy use in python scripts and command line scripting
 '''
 
-from . import create_friktion_table
+from .create_friktion_table import create_friktion_table
