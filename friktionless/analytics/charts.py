59443adcb703f2c646aa27a90f80f83f3330fb93
Matt Martin <matt.martin@hey.com>
2022-04-06 02:58:47 -0700

    added logic for cumulative and new users charts

--- friktionless/analytics/charts.py
+++ friktionless/analytics/charts.py
@@ -39,7 +39,8 @@ def create_net_funds_flow_chart(friktion_gcloud_project, product_name):
                 labelPadding=10,
                 titleFontSize=16,
                 titlePadding=20,
-                grid=True
+                grid=True,
+                ticks=False
                 )
         ),
         color = alt.value('black'),
@@ -78,7 +79,8 @@ def create_net_funds_flow_chart(friktion_gcloud_project, product_name):
                 labelPadding=10,
                 titleFontSize=16,
                 titlePadding=20,
-                grid=True
+                grid=True,
+                ticks=False
                 )
         ),
         tooltip=[
@@ -118,7 +120,8 @@ def create_net_funds_flow_chart(friktion_gcloud_project, product_name):
                 labelPadding=10,
                 titleFontSize=16,
                 titlePadding=20,
-                grid=True
+                grid=True,
+                ticks=False
                 )
         ),
         color = alt.condition(
@@ -157,3 +160,125 @@ def create_net_funds_flow_chart(friktion_gcloud_project, product_name):
     
 
     return final_chart.configure_concat(spacing=50).configure_view(strokeOpacity=0).configure_axisY(domainOpacity=0)
+
+
+def create_cumulative_users_chart(friktion_gcloud_project, strategy, volt_number, asset, voltage):
+    '''
+    Create a line chart which shows the cumulative number of unique users by epoch, segmented by product name, for a supplied google cloud project, strategy, volt number, and asset.
+
+    Example
+    ----------
+    import friktionless as fless
+    fless.analytics.create_cumulative_users_chart('some_project_name','Covered Call',1,'SOL')
+
+    '''
+    # Open net_funds_flow SQL query
+    with open ('friktionless/queries/cumulative_users_by_product_strategy_volt.sql') as query:
+        query_string = query.read()
+
+    # Read in data from Google BigQuery
+    df = pd.read_gbq(query=query_string.format(strategy=strategy, volt_number=volt_number, asset=asset, voltage=voltage), project_id=friktion_gcloud_project)
+
+    # Create Altair charts
+    cumulative_users = alt.Chart(df).mark_line(point=True).encode(
+        x=alt.X(
+            'epoch:O',
+            axis=alt.Axis(
+                title='Epoch',
+                labelAngle=0,
+                labelFontSize=12,
+                labelPadding=10,
+                titleFontSize=16,
+                titlePadding=20
+            )
+        ),
+        y=alt.Y(
+            'cumulative_unique_users',
+            axis=alt.Axis(
+                title='Cumulative Users',
+                labelFontSize=12,
+                labelPadding=10,
+                titleFontSize=16,
+                titlePadding=20,
+                ticks=False
+            )
+        ),
+        color=alt.Color(
+            'product_name',
+            legend=alt.Legend(
+                orient='bottom',
+                columns=7,
+                title='Product Name'
+            )
+        ),
+        tooltip=[
+            alt.Tooltip(
+                'product_name',
+                title='Product Name'
+            ),
+            alt.Tooltip(
+                'cumulative_unique_users',
+                title='Cumulative Unique Users',
+                format=','
+            )
+        ]
+    ).properties(
+        height=400,
+        width=560
+    )
+
+    new_users = alt.Chart(df).mark_bar().encode(
+        x=alt.X(
+            'epoch:O',
+            axis=alt.Axis(
+                title='Epoch',
+                labelAngle=0,
+                labelFontSize=12,
+                labelPadding=10,
+                titleFontSize=16,
+                titlePadding=20
+            )
+        ),
+        y=alt.Y(
+            'sum(epoch_new_users)',
+            axis=alt.Axis(
+                title='New Users',
+                labelFontSize=12,
+                labelPadding=10,
+                titleFontSize=16,
+                titlePadding=20,
+                ticks=False
+            )
+        ),
+        color=alt.Color(
+            'product_name',
+            legend=alt.Legend(
+                orient='bottom',
+                columns=3,
+                title='Product Name'
+            )
+        ),
+        order=alt.Order(
+        'sum(epoch_new_users)',
+        sort='descending'
+        ),
+        tooltip=[
+            alt.Tooltip(
+                'product_name',
+                title='Product Name'
+            ),
+            alt.Tooltip(
+                'epoch_new_users',
+                title='New Users',
+                format=','
+            )
+        ]
+    ).properties(
+        height=400,
+        width=560
+    )
+    
+    # Combine charts together into a single view
+    final_chart = cumulative_users | new_users
+
+    return final_chart.configure_concat(spacing=50)
--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,13 @@ import friktionless as fless
 import streamlit as st
 import pandas as pd
 
+### UTILITY FUNCTION - Needs to be put into friktionless ###
+def open_file(path):
+    with open (path) as query:
+        query_string = query.read()
+    
+    return query_string
+
 st.set_page_config(
     page_title='Friktion Analytics Hub',
     layout='wide',
@@ -10,12 +17,31 @@ st.set_page_config(
 
 st.title('Friktion Analytics Hub')
 
+# Cumulative and New Users Container
+with st.container():
+    st.header('Cumulative and New Users')
+
+    col1, col2, col3, col4 = st.columns(4)
+    
+    with col1:
+        strategy = st.multiselect('Strategy:', list(pd.read_gbq(query=open_file('friktionless/queries/distinct_strategies.sql'), project_id='friktion-dev')['strategy']),default='All')
+    with col2:
+        volt_number = st.multiselect('Volt Number:', list(pd.read_gbq(query=open_file('friktionless/queries/distinct_volt_numbers.sql'), project_id='friktion-dev')['volt_number']),default='All')
+    with col3:
+        asset = st.multiselect('Asset:', list(pd.read_gbq(query=open_file('friktionless/queries/distinct_assets.sql'), project_id='friktion-dev')['strategy']),default='All')
+    with col4:
+        voltage = st.multiselect('Voltage:', list(pd.read_gbq(query=open_file('friktionless/queries/distinct_voltages.sql'), project_id='friktion-dev')['strategy']),default='All')
+
+    st.markdown('#')
+    st.altair_chart(
+        fless.analytics.charts.create_cumulative_users_chart('friktion-dev',strategy,volt_number,asset,voltage),
+        use_container_width=True
+    )
+
+# Net Funds Flow Container
 with st.container():
     st.header('Net Funds Flow')
-    with open ('friktionless/queries/distinct_product_names.sql') as query_product_names:
-        query_string_product_names = query_product_names.read()
-        
-    product_name = st.selectbox('Product Name:', list(pd.read_gbq(query=query_string_product_names, project_id='friktion-dev')['product_name']))
+    product_name = st.selectbox('Product Name:', list(pd.read_gbq(query=open_file('friktionless/queries/distinct_product_names.sql'), project_id='friktion-dev')['product_name']))
     
     st.markdown('#')
     st.altair_chart(
