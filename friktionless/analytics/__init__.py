39123c40fdf5ece18a0765868d241b11492fc434
Amarante <amar@mrnt.xyz>
2022-04-05 23:31:51 -0500

    added analytics api doc in the submodule

--- friktionless/analytics/__init__.py
+++ friktionless/analytics/__init__.py
@@ -1,1 +1,9 @@
+'''
+The Analytics API
+
+This API makes analysis friktionless. It provides tools that facilitate
+ the creation of common charts and parameterized queries to enable 
+ scaled democratization of Friktion data. 
+'''
+
 from . import charts
