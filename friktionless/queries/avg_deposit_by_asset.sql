03080603bafcbd9f76d741008c0e8ceea36abdcb
Matt Martin <matt.martin@hey.com>
2022-04-06 23:44:46 -0700

    initial commit

---
+++ friktionless/queries/avg_deposit_by_asset.sql
@@ -0,0 +1,11 @@
+select
+    case when pc.deposited_asset = 'tsUSDC' then 'SOL [tsUSDC]' else pc.asset end as asset,
+    pc.volt_number,
+    avg(fd.deposit_initiated_amt) as avg_deposit_initiated_amt
+from `friktion-dev.transactions.fact_deposits` fd
+    left join `friktion-dev.analytics.product_catalog` pc on pc.product_name = fd.product_name
+where
+    pc.volt_number = '{}' and
+    asset in ({})
+group by 1,2
+order by 2,1
