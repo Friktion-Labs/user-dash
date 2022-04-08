7cce8cf49c867a8185a7085b956577f64ddf6bf4
Matt Martin <matt.martin@hey.com>
2022-04-07 23:54:12 -0700

    modified dashboard query calls for friktion prod project

--- streamlit.py
+++ streamlit.py
@@ -26,27 +26,27 @@ with st.container():
     with col1:
         strategy = st.multiselect(
             'Strategy:', 
-            list(pd.read_parquet('gs://friktion-strategy/strategy.parquet')['strategy']),
-            list(pd.read_parquet('gs://friktion-strategy/strategy.parquet')['strategy']))
+            list(pd.read_parquet('gs://friktion-strategies-prod/strategies.parquet')['strategy']),
+            list(pd.read_parquet('gs://friktion-strategies-prod/strategies.parquet')['strategy']))
     with col2:
         volt_number = st.multiselect(
             'Volt Number:', 
-            list(pd.read_parquet('gs://friktion-volt-number/volt-number.parquet')['volt_number']),
-            list(pd.read_parquet('gs://friktion-volt-number/volt-number.parquet')['volt_number']))
+            list(pd.read_parquet('gs://friktion-volt-numbers-prod/volt-numbers.parquet')['volt_number']),
+            list(pd.read_parquet('gs://friktion-volt-numbers-prod/volt-numbers.parquet')['volt_number']))
     with col3:
         voltage = st.multiselect(
             'Voltage:', 
-            list(pd.read_parquet('gs://friktion-voltage/voltage.parquet')['voltage']),
-            list(pd.read_parquet('gs://friktion-voltage/voltage.parquet')['voltage']))
+            list(pd.read_parquet('gs://friktion-voltages-prod/voltages.parquet')['voltage']),
+            list(pd.read_parquet('gs://friktion-voltages-prod/voltages.parquet')['voltage']))
 
     asset = st.multiselect(
         'Asset:', 
-        list(pd.read_parquet('gs://friktion-assets/assets.parquet')['asset']),
-        list(pd.read_parquet('gs://friktion-assets/assets.parquet')['asset']))
+        list(pd.read_parquet('gs://friktion-assets-prod/assets.parquet')['asset']),
+        list(pd.read_parquet('gs://friktion-assets-prod/assets.parquet')['asset']))
 
     #st.markdown('#')
     st.altair_chart(
-        fless.analytics.charts.create_cumulative_users_chart('friktion-dev',strategy,volt_number,asset,voltage),
+        fless.analytics.charts.create_cumulative_users_chart('lyrical-amulet-337502',strategy,volt_number,asset,voltage),
         use_container_width=True
     )
 
@@ -55,11 +55,11 @@ with st.container():
     st.header('Net Funds Flow')
     product_name = st.selectbox(
         'Product Name:', 
-        list(pd.read_parquet('gs://friktion-product-catalog/product-catalog.parquet')['product_name'])
+        list(pd.read_parquet('gs://friktion-products-prod/products.parquet')['product_name'])
         )
     
     st.markdown('#')
     st.altair_chart(
-        fless.analytics.charts.create_net_funds_flow_chart('friktion-dev',product_name),
+        fless.analytics.charts.create_net_funds_flow_chart('lyrical-amulet-337502',product_name),
         use_container_width=True
     )
