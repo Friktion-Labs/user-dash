c579e82efc31519d87084770e7f0663d5cffd941
Matt Martin <matt.martin@hey.com>
2022-04-19 22:18:10 -0700

    *actually* commit the change

--- friktionless/analytics/__init__.py
+++ friktionless/analytics/__init__.py
@@ -6,8 +6,6 @@ This API makes analysis friktionless. It provides tools that facilitate
  the creation of common charts and parameterized queries to enable 
  scaled democratization of Friktion data. 
 '''
-
-from . import charts
 =======
 from . import charts, data
 >>>>>>> master
