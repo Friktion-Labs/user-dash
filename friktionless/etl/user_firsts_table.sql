7d6dc1f7e82a1da492531beafc8d7b9ab8e39abf
Amarante <amar@mrnt.xyz>
2022-04-21 00:56:36 +0000

    final loop converted to json lifting to prod

--- friktionless/etl/user_firsts_table.sql
+++ friktionless/etl/user_firsts_table.sql
@@ -4,11 +4,11 @@ select
     fd.user_address
     , fd.first_deposit_date
     , fd.first_deposit_epoch
-    , fd.first_deposit_token.list as first_deposit_token
+    , fd.first_deposit_token as first_deposit_token
     , fd.first_deposit_amount
     , fw.first_withdrawal_date
     , fw.first_withdrawal_epoch
-    , fw.first_withdrawal_token.list as first_withdrawal_token
+    , fw.first_withdrawal_token as first_withdrawal_token
     , fw.first_withdrawal_amount
 from `friktion-dev.users.fact_user_first_deposit` fd
 left join `friktion-dev.users.fact_user_first_withdrawal` fw
